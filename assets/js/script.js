39f52e7c9c250acca0c625f94d4b1a340c1b24b4
tielkingc <tielking5@gmail.com>
2021-05-07 15:22:05 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,15 +78,23 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log(ui);
   },
   deactivate: function(event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     console.log(ui);
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
+    
     console.log(event);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
+    
     console.log(event);
   },
   update: function() {
@@ -133,9 +141,11 @@ $("#trash").droppable({
 
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log(ui);
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log(ui);
   }
 });
@@ -154,7 +164,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -286,3 +296,8 @@ $("#modalDueDate").datepicker({
   minDate: 1
 });
 
+setInterval(function() {
+  $(".car .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 5000);
